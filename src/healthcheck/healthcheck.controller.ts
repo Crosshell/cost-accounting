f78509fe4f160d777101ccdfc6e0246a85d19217
Crosshell <confighell@gmail.com>
2025-09-29 18:01:22 +0300

    feat: integrate Swagger for API documentation and enhance healthcheck responses with metadata

--- src/healthcheck/healthcheck.controller.ts
+++ src/healthcheck/healthcheck.controller.ts
@@ -1,8 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
 import { ServerStatus } from './types/server-status';
+import { ApiOkResponse } from '@nestjs/swagger';
 
 @Controller('healthcheck')
 export class HealthcheckController {
+  @ApiOkResponse({ type: ServerStatus })
   @Get()
   getHealthcheck(): ServerStatus {
     return {
--- src/healthcheck/types/server-status.ts
+++ src/healthcheck/types/server-status.ts
@@ -1,4 +1,9 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class ServerStatus {
+  @ApiProperty({ example: '2025-09-29T14:51:39.938Z' })
   date: Date;
+
+  @ApiProperty({ example: 'ok' })
   status: string;
 }
