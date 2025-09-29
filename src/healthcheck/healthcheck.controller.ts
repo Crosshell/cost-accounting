3a73c2da6e4f2115157f5c28f32d87f6dab3f1c2
Crosshell <confighell@gmail.com>
2025-09-29 17:44:12 +0300

    feat: add ServerStatus type and update healthcheck endpoint to return status object

--- src/healthcheck/healthcheck.controller.ts
+++ src/healthcheck/healthcheck.controller.ts
@@ -1,9 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
+import { ServerStatus } from './types/server-status';
 
 @Controller('healthcheck')
 export class HealthcheckController {
   @Get()
-  getStatus(): string {
-    return 'Hello World!';
+  getHealthcheck(): ServerStatus {
+    return {
+      date: new Date(),
+      status: 'ok',
+    };
   }
 }
