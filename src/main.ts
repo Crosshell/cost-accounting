ecfbeae1df5ec2b8299f539eedd07ecc4faa1538
Crosshell <confighell@gmail.com>
2025-10-01 14:33:09 +0300

    feat: add validation to CreateUserDto and enable global ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const configService = app.get(ConfigService);
   const port = configService.getOrThrow<number>('port');
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   const swaggerConfig = new DocumentBuilder()
     .setTitle('Cost accounting docs')
