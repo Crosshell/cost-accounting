9f8854f72f1a351fd2b05ab624c666867e1331ea
Crosshell <confighell@gmail.com>
2025-10-01 13:49:14 +0300

    feat: implement base CRUD endpoints in UserController

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post } from '@nestjs/common';
 
-@Controller('user')
-export class UserController {}
+@Controller()
+export class UserController {
+  @Get('user/:id')
+  getUserById(@Param('id') id: string) {}
+
+  @Delete('user/:id')
+  deleteUserById(@Param('id') id: string) {}
+
+  @Post('user')
+  createUser() {}
+
+  @Get('users')
+  getAllUsers() {}
+}
