c8190bcbb039b059de5720600406fdcab6cda495
Crosshell <confighell@gmail.com>
2025-10-08 16:53:34 +0300

    feat: refactor Category module to use Prisma as ORM and add validation

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,25 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CategoryRepository } from './category.repository';
-import { Category } from './entities/category';
+import { Category } from '@prisma/client';
 import { CreateCategoryDto } from './dto/create-category.dto';
 
 @Injectable()
 export class CategoryService {
   constructor(private readonly repository: CategoryRepository) {}
 
-  getAll(): Category[] {
+  async getAll(): Promise<Category[]> {
     return this.repository.getAll();
   }
 
-  getById(id: string): Category | string {
-    return this.repository.getById(id);
+  async findOne(id: string): Promise<Category> {
+    const user = await this.repository.findOne({ id });
+
+    if (!user) {
+      throw new NotFoundException(`Category with id: ${id} not found`);
+    }
+
+    return user;
   }
 
-  create(dto: CreateCategoryDto): Category {
+  async create(dto: CreateCategoryDto): Promise<Category> {
     return this.repository.create(dto);
   }
 
-  deleteById(id: string): string {
-    return this.repository.deleteById(id);
+  async delete(id: string): Promise<Category> {
+    const user = await this.repository.findOne({ id });
+
+    if (!user) {
+      throw new NotFoundException(`Category with id: ${id} not found`);
+    }
+
+    return this.repository.delete({ id });
   }
 }
--- src/category/entities/category.ts
+++
@@ -1,4 +0,0 @@
-export class Category {
-  id: number | string;
-  name: string;
-}
