e8111c7f2bb824b6e82c124525b34ea93088246e
Crosshell <confighell@gmail.com>
2025-10-08 21:52:19 +0300

    feat: add userId filtering for Category retrieval

--- src/category/dto/create-category.dto.ts
+++ src/category/dto/create-category.dto.ts
@@ -1,8 +1,18 @@
-import { IsNotEmpty, IsString, Length } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsUUID,
+  Length,
+} from 'class-validator';
 
 export class CreateCategoryDto {
   @IsNotEmpty()
   @IsString()
   @Length(2, 30)
   name: string;
+
+  @IsOptional()
+  @IsUUID()
+  userId?: string;
 }
